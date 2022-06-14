3fa56d5649a64669c2608eef6fc7f7d99fae4f1d
nunopcfmarques <nuno.figueiredo.marques@tecnico.ulisboa.pt>
2022-06-14 19:18:32 +0100

    Added an executables folder which stores our debug mode .txt files. Added two new methods to the Hand Class. One is used to read a card from the card-file and the other is used to add these new read cards to the hand. Improved switch cases.

--- src/pok1/Hand.java
+++ src/pok1/Hand.java
@@ -1,38 +1,47 @@
 package pok1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Hand extends SetOfCards{
 
 	public Hand(String cards[]) {
 		super(cards);
 	}
 	
+	public String readCard(File file, int index) throws FileNotFoundException {
+		Scanner scan = new Scanner(file);
+		String cardfile = scan.nextLine();
+		String[] readCards = cardfile.split(" ");
+		return readCards[index];
+		
+	}
 	
 	public void draw(int numDrawn, Deck deck) {
+		
 		System.out.println("In draw()");
 		int prevNumCards = numCards;
-
+	
 		String drawn[] = new String[numDrawn];
 		drawn = deck.deal(numDrawn);
-		
+			
 		// "Deals" cards from the hand and does nothing with them. Basically removing with the tidyness 
 		// of "deal"
 		deal(numDrawn);
-		
+			
 		//System.out.println("NumCards is " + numCards);
-		
+			
 		// Fills emptied spots with cards drawn from the deck
 		for(int i = numDrawn - 1; i >= 0; i--) {
 			cards[prevNumCards - i - 1] = drawn[i];
 			numCards++;
 		}
-		
 	}
 	
-	/*
-	public void discard(int index, Deck deck) {
-		cards[index]=deck.deal(1)[0];
-
+	public void dDraw(int pos, File file) throws FileNotFoundException {
+		for (int i = 0; i<5;i++) {
+			cards[i]=readCard(file,pos+i);
+		}
 	}
-	*/
-
 }
--- src/pok1/Poker.java
+++ src/pok1/Poker.java
@@ -12,42 +12,51 @@ public class Poker {
 		Deck deck;
 		Player player;
 		
-		/*
-		File cmd = new File (System.getProperty("user.dir") + "\\src\\pok1\\" + "cmd-file.txt");
-		Scanner scan = new Scanner(cmd);
-		String cmds=scan.nextLine();
-				
-		for (int i = 0; i<cmds.length(); i++) {
-			char c = cmds.charAt(i);
-			switch(c) {
-				case 'b':
-					System.out.println(" b");
-					break;
-				case '$':
-					System.out.println("$");
-					break;
-				case 'd':
-					System.out.println(" d");
-					break;
-				case 'h':
-					System.out.println(" h");
-					break;
-				case 'a':
-					System.out.println(" a");
-					break;
-				case 's':
-					System.out.println(" s");
-					break;
-				 default:
-					 break;
+		if (args[0].equals("-d")){
+			String cards[] = new String[5];
+			deck = new Deck();
+			player = new Player(Integer.parseInt(args[1]),cards);
+			File cmds = new File (System.getProperty("user.dir")+ "\\" + args[2]+".txt");
+			File filecards = new File (System.getProperty("user.dir") + "\\" + args[3]+".txt");
+			Scanner scan = new Scanner(cmds);
+			String cmd=scan.nextLine();
+			int pos=0;
+					
+			for (int i = 0; i<cmds.length(); i++) {
+				char c = cmd.charAt(i);
+				switch(c) {
+					case 'b':
+						break;
+					case '$':
+						break;
+					case 'd':
+						player.hand.dDraw(pos, filecards);
+						player.hand.printCards();
+						pos+=5;
+						break;
+					case 'h':
+						pos+=3;
+						break;
+					case 'a':
+						break;
+					case 's':
+						break;
+					 default:
+						 break;
+				}
 			}
 		}
-		*/
 		
+		else {
+			return;
+		}
+		
+		/*
 		deck = new Deck();
 		player = new Player(1000,deck.deal(5));
 		player.bet(10);
 		player.showCredit();
 		player.hand.printCards();
+		*/
 	}
 }
