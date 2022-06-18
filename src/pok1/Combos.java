e7efdc554fa9bf73a9a0656ab3ad709ae9706e0a
SebastiaoCaldas <sebastiaolemoscaldas@hotmail.com>
2022-06-18 22:43:40 +0100

    ahhaahahahahhahahhahahahhahaha myLittleBranchyBranch commit number 1 <3

--- src/pok1/Combos.java
+++ src/pok1/Combos.java
@@ -1,5 +1,7 @@
 package pok1;
 
+import java.util.zip.ZipEntry;
+
 /*
  * AS FUNÇÕES DESTE FICHEIRO TRATAM DE COMBINAÇÕES DE 5 CARTAS E ASSUMEM QUE VÊM ORDENADAS 
  * POR ORDEM DECRESCENTE (E ALFABÉTICA)
@@ -28,12 +30,13 @@ public final class Combos {
 	 */
 
 	public static int[] checkForPair(String cards[]){
-		int flagAndValue[] = {0,0};
+		int flagAndValue[] = {-1,-1,-1};
 	    for(int i = 0; i < 4; i++){
 	        if(cards[i].charAt(0) == cards[i+1].charAt(0)){
 	            // flagAndValue[0] = 1 revela que há um par no conjunto (=0 revela que não) e flagAndValue[1] revela o valor das cartas desse par
 	            flagAndValue[0] = 1;
 	            flagAndValue[1] = getCardValue(cards[i]);
+	            flagAndValue[2] = i;
 	            return flagAndValue;
 	        }
 	    }
@@ -47,12 +50,13 @@ public final class Combos {
 	// um par de valor diferente do valor do trio (forbiddenValue)
 
 	public static int[] checkForPair2(String cards[], int forbiddenValue){
-		int flagAndValue[] = {0,0};
+		int flagAndValue[] = {-1,-1,-1};
 	    for(int i = 0; i < 4; i++){
 	        if((cards[i].charAt(0) == cards[i+1].charAt(0)) && (cards[i].charAt(0) != values[forbiddenValue])){
 	            // flagAndValue[0] = 1 revela que h� um par no conjunto (=0 revela que n�o) e flagAndValue[1] revela o valor das cartas desse par.
 	            flagAndValue[0] = 1;
 	            flagAndValue[1] = getCardValue(cards[i]);
+	            flagAndValue[2] = i;
 	            return flagAndValue;
 	        }
 	    }
@@ -66,7 +70,7 @@ public final class Combos {
 	// cura um par com cartas de valor diferente.
 
 	public static int[] checkForTwoPair(String cards[]){
-		int flagAndValue3[] = {0,0,0};
+		int flagAndValue3[] = {-1,-1,-1,-1,-1};
 	    // flagAndValue[0] = 1 revela que h� um par no conjunto (=0 revela que n�o) e flagAndValue[1] revela o valor das cartas desse par.
 	    // Este flagAndValue � tempor�rio, pelo que n�o � um argumento e � declarado dentro da fun��o.
 	    int flagAndValue[] = checkForPair(cards);
@@ -74,6 +78,7 @@ public final class Combos {
 
 	        // guarda o valor do par, que ser� o par mais alto do conjunto.
 	        int max = flagAndValue[1];
+	        flagAndValue3[3] = flagAndValue[2]; // A confiar que não se olha para aqui exceto se a flag principal for 1
 
 	        // Procura um segundo par de valor diferente de max (o valor das cartas do primeiro par).
 	        flagAndValue = checkForPair2(cards, max);
@@ -83,6 +88,7 @@ public final class Combos {
 	            flagAndValue3[0] = 1;
 	            flagAndValue3[1] = max;
 	            flagAndValue3[2] = flagAndValue[1];
+	            flagAndValue3[4] = flagAndValue[2];
 	            return flagAndValue3;
 
 	        }
@@ -98,12 +104,13 @@ public final class Combos {
 	// Semelhante a checkForPair, mas verifica tr�s cartas seguidas do mesmo valor, em vez de duas.
 
 	public static int[] checkForThreeOfAKind(String cards[]){
-		int flagAndValue[] = {0,0};
+		int flagAndValue[] = {-1,-1,-1}; 
 	    for(int i = 0; i < 3; i++){
 	        if(getCardValue(cards[i]) == getCardValue(cards[i+1]) && getCardValue(cards[i+1]) == getCardValue(cards[i+2])){
 	            // flagAndValue[0] = 1 revela que h� um par no conjunto (=0 revela que n�o) e flagAndValue[1] revela o valor das cartas desse trio.
 	            flagAndValue[0] = 1;
 	            flagAndValue[1] = getCardValue(cards[i]);
+	            flagAndValue[2] = i;
 	            return flagAndValue;
 	        }
 	    }
@@ -160,13 +167,14 @@ public final class Combos {
 	// Verifica se h� um par e se h� um trio, e se o par � de valor diferente do valor do trio. (Usa checkForPair2)
 
 	public static int[] checkForFullHouse(String cards[]){
-		int flagAndValue3[] = {0,0,0};
+		int flagAndValue3[] = {-1,-1,-1,-1,-1}; // index 4: first card of pair. index 5: first of ToaK
 	    int flagAndValue[] = checkForThreeOfAKind(cards);
 	    int a;
 	    
 	    if(flagAndValue[0] == 1){
 	        // Guarda o valor do trio
 	        a = flagAndValue[1];
+	        flagAndValue3[4] = flagAndValue[2];
 	    }
 	    else{
 	        // Se n�o houver um trio, p�e flagAndValue3 a zeros.
@@ -182,6 +190,7 @@ public final class Combos {
 	        flagAndValue3[0] = 1;
 	        flagAndValue3[1] = a;
 	        flagAndValue3[2] = flagAndValue[1];
+	        flagAndValue3[3] = flagAndValue[2];
 	        return flagAndValue3;
 	    }
 	    // Se n�o houver um par de valor diferente  do valor do trio, p�e flagAndValue3 a zeros.
@@ -194,13 +203,14 @@ public final class Combos {
 	// Semelhante a checkForPair e a checkForThreeOfAKind, mas verifica se quatro cartas seguidas t�m o mesmo valor.
 
 	public static int[] checkForFourOfAKind(String cards[]){
-		int flagAndValue[] = {0,0};
+		int flagAndValue[] = {-1,-1,-1};
 	    for(int i = 0; i < 2; i++){
 	        if(getCardValue(cards[i]) == getCardValue(cards[i+1]) && getCardValue(cards[i+1]) == getCardValue(cards[i+2]) &&
 	           getCardValue(cards[i+2]) == getCardValue(cards[i+3])){
 	            // flagAndValue[0] = 1 revela que h� um par no conjunto (=0 revela que n�o) e flagAndValue[1] revela o valor das cartas dessa quadrupla.
 	            flagAndValue[0] = 1;
 	            flagAndValue[1] = getCardValue(cards[i]);
+	            flagAndValue[2] = i;
 	            return flagAndValue;
 	        }
 	    }
@@ -321,5 +331,388 @@ public final class Combos {
 	    // Se nenhuma das combina��es anteriores estiver presente, � uma m�o de for�a High Card. Esta � a defini��o de High Card.
 	    return 1;
 	}
+	
+	public static int[] checkForSFD(String cards[]) {
+		int flagAndValue[] = {0,0};
+		
+		return flagAndValue;
+	}
+	
+	// There is a danger in not checking immediately for a flush! You might return the first 4toS that you find, which might not be a 4toSF, but there might be another 4toS which is a 4toSF
+	public static int[] checkFor4toS(String cards[]) {
+	    int a = getCardValue(cards[0]);
+	    int b = getCardValue(cards[1]);
+	    int c = getCardValue(cards[2]);
+	    int d = getCardValue(cards[3]);
+	    int e = getCardValue(cards[4]);
+	    
+	    // If I'm already checking for Flush, do I need to return indices?
+	    int flagAndValue[] = {0, 0, 0, 0, 0, 0, 0}; //isThere, type (1=outside,0=inside), isItFlush, indices(4)
+	    int tempIndices[] = {0,0,0,0};
+	    // Criar arrays para terem versoes piores (mas que dao mais jeito organizar da forma atual na funçao)
+	    // Tipo ter um flagAndValueOutsideNoFlush, para assim que virmos que nao ha um insideflush darmos return a esta
+	    int flagAndValueONF[] = {0,0,0,0,0,0,0};
+	    int flagAndValueINF[] = {0,0,0,0,0,0,0};
+	    
+	    
+	    // Outside straights
+	    int array[]={a,b,c,d};	
+    	int array2[] ={a,b,c,e};    	
+    	int array3[] ={a,b,d,e};
+    	int array4[] ={a,c,d,e};
+    	int array5[] ={b,c,d,e};
+    	int arrays[][] = {array, array2, array3, array4, array5};
+    	
+		int newa = b, newb = c, newc = d, newd = e, newe = -1;
+		if(a!=12) {
+			newa = a;
+			newb = b;
+			newc = c;
+			newd = d;
+			newe = e;
+		}
+	    int narray[]={newa,newb,newc,newd};	
+    	int narray2[] ={newa,newb,newc,newe};    	
+    	int narray3[] ={newa,newb,newd,newe};
+    	int narray4[] ={newa,newc,newd,newe};
+    	int narray5[] ={newb,newc,newd,newe};
+    	int narrays[][] = {narray, narray2, narray3, narray4, narray5};
+    	
+		String movedCards[] = new String[5];
+		movedCards[0] = cards[1];
+		movedCards[1] = cards[2];
+		movedCards[2] = cards[3];
+		movedCards[3] = cards[4];
+		movedCards[4] = cards[0];
+    	
+    	
+    	if(a!= 12) {
+    		
+    		for(int k = 0; k < 5; k++) {
+    			if(areConsecutive(arrays[k])) {
+    				String theseCards[] = new String[4];
+    				
+    				int cnt = 0;
+    				for(int l = 0; l < 5; l++) {
+    					if(l != 4-k) {
+    						theseCards[cnt] = cards[l];
+    						tempIndices[cnt] = l;
+    						cnt++;
+    					}
+    				}
+    				if(areFlushy(theseCards)) {
+    					// It's ok to return it because this is the best possible scenario 
+    					flagAndValue[0] =  1;
+    					flagAndValue[1] =  1;
+    					flagAndValue[2] =  1;
+    					
+    				
+    					flagAndValue[3] =  tempIndices[0];
+    					flagAndValue[4] =  tempIndices[1];
+    					flagAndValue[5] =  tempIndices[2];
+    					flagAndValue[6] =  tempIndices[3];
+    					
+    					return flagAndValue;
+    					
+    				}
+    				else {
+    					flagAndValueONF[0] =  1;
+    					flagAndValueONF[1] =  1;
+    					
+    					flagAndValueONF[3] =  tempIndices[0];
+    					flagAndValueONF[4] =  tempIndices[1];
+    					flagAndValueONF[5] =  tempIndices[2];
+    					flagAndValueONF[6] =  tempIndices[3];	
+					}
+    			}
+    		}
+
+    	}
+	    /*
+	    else if ((d==e) && areConsecutive(array)  && (a!=12)) {
+	    	return flagAndValue;
+	    }*/
+	    
+	    
+	    // Inside straights
+	    
+	    // ==4 e depois if(a-b) == -1 {verificar se a gap está em b-c, else...} else está aí a gap, verificar se é sequência no resto
+	    //int arrayT[] = {a,b,c,d,e};
+
+
+    	else {
+    		
+    		if(b != 12) {
+    			String theseCards[] = new String[4];
+    			if(areConsecutive(arrays[4])) {
+    				for(int l = 1; l < 5; l++) {
+    					theseCards[l-1] = cards[l];
+    				}
+    				
+    				if(areFlushy(theseCards)) {
+    					// It's ok to return it because this is the best possible scenario 
+    					flagAndValue[0] =  1;
+    					flagAndValue[1] =  1;
+    					flagAndValue[2] =  1;
+    					
+    				
+    					flagAndValue[3] =  1;
+    					flagAndValue[4] =  2;
+    					flagAndValue[5] =  3;
+    					flagAndValue[6] =  4;
+    					
+    					return flagAndValue;
+    					
+    				}
+    				else {
+    					flagAndValueONF[0] =  1;
+    					flagAndValueONF[1] =  1;
+    					
+    					flagAndValueONF[3] =  tempIndices[0];
+    					flagAndValueONF[4] =  tempIndices[1];
+    					flagAndValueONF[5] =  tempIndices[2];
+    					flagAndValueONF[6] =  tempIndices[3];	
+					}
+    			}
+    		}
+    		
+    		
+    		
+    		
+    		
+    		
+    		for(int l = 0; l < 4; l++) { // Only goes up to 4 because the last array doesn't have the ace!
+    			if(areConsecutive(arrays[l])) {
+    				String theseCards[] = new String[4];
+    				
+    				int cnt = 0;
+    				for(int p = 0; p < 5; p++) {
+    					if(p != 4-l) {
+    						theseCards[cnt] = cards[p];
+    						tempIndices[cnt] = p;
+    						cnt++;
+    					}
+    				}
+    				if(areFlushy(theseCards)) {
+    					// It's ok to change it because this is the best possible scenario
+    					System.out.println("Here");
+    					flagAndValue[0] =  1;
+    					flagAndValue[1] =  0;
+    					flagAndValue[2] =  1;
+    					
+    				
+    					flagAndValue[3] =  tempIndices[0];
+    					flagAndValue[4] =  tempIndices[1];
+    					flagAndValue[5] =  tempIndices[2];
+    					flagAndValue[6] =  tempIndices[3];
+    					return flagAndValue;
+    					
+    				}
+    				else {
+    					flagAndValueINF[0] =  1;
+    					
+    					flagAndValueINF[3] =  tempIndices[0];
+    					flagAndValueINF[4] =  tempIndices[1];
+    					flagAndValueINF[5] =  tempIndices[2];
+    					flagAndValueINF[6] =  tempIndices[3];
+					}
+				}
+    		}
+    		
+    		
+    		
+    		
+    		// I'm not sure if this checks everything left...
+
+    		
+
+    		for(int l = 1; l < 5; l++) { // Only goes from 2 because the first array doesn't have the ace!
+    			if(areConsecutive(narrays[l])) {
+    				String theseCards[] = new String[4];
+    				int cnt = 0;
+    				// Esta brincadeira fica diferente porque os narrays nao correspondem às ordens normais!
+    				// Agora talvez dê com o uso de um outro "cards"
+    				for(int p = 0; p < 5; p++) {
+    					if(p != 4-l) {
+    						theseCards[cnt] = movedCards[p];
+    						tempIndices[cnt] = p;
+    						cnt++;
+    					}
+    				}
+    				if(areFlushy(theseCards)) {
+    					// It's ok to change it because this is the best possible scenario
+    					flagAndValue[0] =  1;
+    					flagAndValue[1] =  0;
+    					flagAndValue[2] =  1;
+    					
+    				
+    					flagAndValue[3] =  tempIndices[0];
+    					flagAndValue[4] =  tempIndices[1];
+    					flagAndValue[5] =  tempIndices[2];
+    					flagAndValue[6] =  tempIndices[3];
+    					
+    					String tempCard = cards[0];
+    		    		cards[0] = cards[1];
+    		    		cards[1] = cards[2];
+    		    		cards[2] = cards[3];
+    		    		cards[3] = cards[4];
+    		    		cards[4] = tempCard;
+    					return flagAndValue;
+    					
+    				}
+    				else {
+    					flagAndValueINF[0] =  1;
+    					a = -1;
+    					flagAndValueINF[3] =  tempIndices[0];
+    					flagAndValueINF[4] =  tempIndices[1];
+    					flagAndValueINF[5] =  tempIndices[2];
+    					flagAndValueINF[6] =  tempIndices[3];
+					}
+    			}
+    		}
+    		
+
+		}
+
+        for (int k = 0; k < 5; k++) {
+        	System.out.println("InHere!");
+	    	for(int i = 0; i < 3; i++) {
+	    		
+		    	if(areConsecutiveWithHole(arrays[k], i, 2)) {
+		    		//System.out.println("sup");
+		    		String theseCards[] = new String[4];
+    				
+    				int cnt = 0;
+    				for(int p = 0; p < 5; p++) {
+    					if(p != 4-k) {
+    						theseCards[cnt] = cards[p];
+    						tempIndices[cnt] = p;
+    						cnt++;
+    					}
+    				}
+    				if(areFlushy(theseCards)) {
+    					// It's ok to change it because this is the best possible scenario
+    					flagAndValue[0] =  1;
+    					flagAndValue[1] =  0;
+    					flagAndValue[2] =  1;
+    					
+    				
+    					flagAndValue[3] =  tempIndices[0];
+    					flagAndValue[4] =  tempIndices[1];
+    					flagAndValue[5] =  tempIndices[2];
+    					flagAndValue[6] =  tempIndices[3];
+    					return flagAndValue;
+    					
+    				}
+    				else {
+    					flagAndValueINF[0] =  1;
+    					
+    					flagAndValueINF[3] =  tempIndices[0];
+    					flagAndValueINF[4] =  tempIndices[1];
+    					flagAndValueINF[5] =  tempIndices[2];
+    					flagAndValueINF[6] =  tempIndices[3];
+					}
+		    	}
+   
+	    	}
+    	}
+        for (int k = 0; k < 5; k++) {
+	    	for(int i = 0; i < 3; i++) {
+	    		
+		    	if(areConsecutiveWithHole(narrays[k], i, 1)) {
+		    		String theseCards[] = new String[4];
+    				
+    				int cnt = 0;
+    				for(int p = 0; p < 5; p++) {
+    					if(p != 4-k) {
+    						theseCards[cnt] = movedCards[p];
+    						tempIndices[cnt] = p;
+    						cnt++;
+    					}
+    				}
+    				if(areFlushy(theseCards)) {
+    					// It's ok to change it because this is the best possible scenario
+    					flagAndValue[0] =  1;
+    					flagAndValue[1] =  0;
+    					flagAndValue[2] =  1;
+    					
+    					String tempCard = cards[0];
+    		    		cards[0] = cards[1];
+    		    		cards[1] = cards[2];
+    		    		cards[2] = cards[3];
+    		    		cards[3] = cards[4];
+    		    		cards[4] = tempCard;
+    		    		
+    					flagAndValue[3] =  tempIndices[0];
+    					flagAndValue[4] =  tempIndices[1];
+    					flagAndValue[5] =  tempIndices[2];
+    					flagAndValue[6] =  tempIndices[3];
+    					return flagAndValue;
+    					
+    				}
+    				else {
+    					flagAndValueINF[0] =  1;
+    					a = -1;
+    					flagAndValueINF[3] =  tempIndices[0];
+    					flagAndValueINF[4] =  tempIndices[1];
+    					flagAndValueINF[5] =  tempIndices[2];
+    					flagAndValueINF[6] =  tempIndices[3];
+					}
+		    	}
+   
+	    	}
+    	}
+        
+        
+        
+        
+        
+		if(flagAndValueONF[0] == 1)
+			return flagAndValueONF;		
+		if(flagAndValueINF[0] == 1) {
+			if (a==-1) {
+				String tempCard = cards[0];
+	    		cards[0] = cards[1];
+	    		cards[1] = cards[2];
+	    		cards[2] = cards[3];
+	    		cards[3] = cards[4];
+	    		cards[4] = tempCard;
+			}
+			return flagAndValueINF;
+		}
+		return flagAndValue;
+	    
+	}
+	
+	public static boolean areConsecutive(int array[]) {
+		int length = array.length;
+		for(int i = 0; i < length-1; i++) {
+			if(array[i] - array[i+1] != 1) return false;
+		}
+		return true;
+	}
+	
+	public static boolean areFlushy(String cards[]) {
+		int length = cards.length;
+		for(int i = 0; i < length-1; i++) {
+			if(cards[i].charAt(1) != cards[i+1].charAt(1)) return false;
+		}
+		return true;
+	}
+	
+	public static boolean areConsecutiveWithHole(int array[], int holeIndex, int holeSize) {
+		int length = array.length;
+		for(int i = 0; i < length-1; i++) {
+			if ((i != holeIndex) && (array[i] - array[i+1] != 1))
+				return false;
+			else if((i == holeIndex) && (array[i] - array[i+1] != holeSize))
+				return false;
+		}
+		return true;
+	}
+	
+	
+	
+	
 
 }
--- src/pok1/Poker.java
+++ src/pok1/Poker.java
@@ -31,7 +31,7 @@ public class Poker {
 		System.out.println("This is the hand's value " + value);	
 		*/
 		int counter = 0;
-		for(i = 0; i < 10000; i++) {
+		for(i = 0; i < 100000; i++) {
 			deck = new Deck();
 			//deck.printDeck();
 			//System.out.println();
@@ -42,13 +42,44 @@ public class Poker {
 			myHand = new Hand(deck.deal(5));
 			myHand.setCards(Combos.sortCards(myHand.getCards()));
 			value = Combos.getHandValue(myHand.cards);
-			if(value == 1) {
+			if(value == 7) {
 				counter++;
 				myHand.printCards();
 			}
 		}
-		System.out.println("Number of " + 10 + "'s: " + counter);
+		System.out.println("Number of " + 5 + "'s: " + counter);
 		
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int myarray[] = {a,b,c};
+		System.out.println(myarray[0]);
+		addOne(myarray);
+		System.out.println(myarray[0]);
+		
+		deck = new Deck();
+		//deck.printDeck();
+		//System.out.println();
+		//Random rdm = new Random();
+		//for(int i = 0; i < 100; i++)
+		//System.out.print(rdm.nextInt(32) + " ");
+		//int numDealt = 23;
+		myHand = new Hand(deck.deal(5));
+		myHand.printCards();
+		reorder(myHand.cards);
+		myHand.printCards();
+		
+		String myCards[] = {"TC", "TS", "9S", "7C", "6S"}; 
+		int myArray[] = Combos.checkFor4toS(myCards);
+		for(int var = 0; var < myArray.length; var++) {
+			System.out.print(myArray[var] + ", ");
+		}
+		System.out.println();
+		for(int var = 0; var < myCards.length; var++) {
+			System.out.print(myCards[var] + ", ");
+		}
+		
+
 		//String dealt[] = deck.deal(numDealt);
 
 		/*
@@ -70,6 +101,18 @@ public class Poker {
 
 	}
 	
+	public static void addOne(int a[]) {
+		int b[] = {100,95};
+		a = b;
+	}
+	public static void reorder(String cards[]) {
+		String tempCard = cards[0];
+		cards[0] = cards[1];
+		cards[1] = cards[2];
+		cards[2] = cards[3];
+		cards[3] = cards[4];
+		cards[4] = tempCard;
+	}
 	
 
 }
